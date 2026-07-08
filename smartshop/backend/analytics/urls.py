b82326418e3b21381b53db6afb190e2a48ef135c
Vignesh-Ajri <ssvignesh2003@gmail.com>
2026-07-08 23:52:29 +0530

    Update smartshop/backend/analytics/urls.py

--- smartshop/backend/analytics/urls.py
+++ smartshop/backend/analytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TrackEventView
+from .views import TrackEventView, RecentEventsView
 
 urlpatterns = [
     path('track/', TrackEventView.as_view(), name='track_event'),
+    path('events/', RecentEventsView.as_view(), name='recent_events'),
 ]
