7814a89b7817b0c4c7630b32973ea8ad26ee26b5
Vignesh-Ajri <ssvignesh2003@gmail.com>
2026-07-08 23:52:52 +0530

    Update smartshop/backend/recommendations/urls.py

--- smartshop/backend/recommendations/urls.py
+++ smartshop/backend/recommendations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CPRPRecommendationView
+from .views import CPRPRecommendationView, ColdStartRecommendationView
 
 urlpatterns = [
     path('<str:user_id>/', CPRPRecommendationView.as_view(), name='get_recommendations'),
+    path('cold/<str:category>/', ColdStartRecommendationView.as_view(), name='cold_start_recommendations'),
 ]
